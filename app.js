58b96f71fcf80c8d50a627721ec567a2f8e2ad29
Tro <noe.monsivais@gmail.com>
2020-04-26 10:04:19 -0500

    added switch statement and add deparment funciton

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
     database: "employee_Tracker"
 });
 
-// connect to the mysql server and sql database
+// connect to the mysql server and sql database ***connecion.query***
 connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
@@ -36,7 +36,27 @@ function start() {
         ])
         //function response will record what choice user responded.
         .then(function (response) {
-            console.log(response);
-            //create if/else statements to go to next question depending on response.
+            switch (response.addViewUpdate) {
+                case "Add a department":
+                    console.log("About to add department")
+                    addDepartment();
+                    break
+            }
+            console.log(response.addViewUpdate);
+            //create switch statements to go to next question depending on response.
         })
+
+}
+
+function addDepartment() {
+    //Add a Department//
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What department would you like to add?",
+                name: "addDepartment"
+            },
+
+        ])
 }
