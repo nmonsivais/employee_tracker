a6b3c0bd8994a9f21fa9485fb05059c2344fbad9
Tro <noe.monsivais@gmail.com>
2020-04-26 00:47:10 -0500

    Created a responsequestions js file to house the questions for response.

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ inquirer
         {
             type: "list",
             message: "What would you like to do?",
-            choices: ["Add a department", "Add a role", "Add an employee", "View a department", "View a role", "View an employee", "Update and employee role",],
+            choices: ["Add a department", "Add a role", "Add an employee", "View a department", "View a role", "View an employee", "Update an employee role",],
             name: "addAction"
         },
 
     ])
+    //function response will record what choice user responded.
     .then(function (response) {
         console.log(response);
-
+        //create if/else statements to go to next question depending on response.
     })
---
+++ responsequestions.js
@@ -0,0 +1,11 @@
+["Add a department", "Add a role", "Add an employee", "View a department", "View a role", "View an employee", "Update an employee role",]
+
+//Add a Department//
+//Add a Role//
+//Add a Employee//
+
+//View a Department//
+//View a Role//
+//View a Employee//
+
+//Update an Employee Role//
