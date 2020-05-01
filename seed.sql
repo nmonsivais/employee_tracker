dc696b3e819dd537f788037bc33da5590f465279
Tro <noe.monsivais@gmail.com>
2020-04-30 23:41:49 -0500

    Updated read me.

--- seed.sql
+++ seed.sql
@@ -1,9 +1,23 @@
 USE employee_Tracker;
 
 INSERT INTO department (dept_id, dept_name)
+VALUES (1, "None");
 
-VALUES (1, "NONE");
+INSERT INTO department (dept_id, dept_name)
+VALUES (2, "Engineering");
 
+INSERT INTO department (dept_id, dept_name)
+VALUES (3, "Sales");
+
+INSERT INTO department (dept_id, dept_name)
+VALUES (4, "Marketing");
+
+INSERT INTO department (dept_id, dept_name)
+VALUES (5, "Accounting");
+
+INSERT INTO department (dept_id, dept_name)
+VALUES (6, "Legal Team");
+-- ----------------------------------------------------------
 INSERT INTO emp_role (role_id, title, salary, department_id)
 VALUES (1, "Sales Lead", 50000, 2);
 
@@ -24,6 +38,21 @@ VALUES (6, "Account Manager", 55000, 7);
 
 INSERT INTO emp_role (role_id, title, salary, department_id)
 VALUES (7, "Legal Team Lead", 100000, 8);
+-- ------------------------------------------------------------
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Brad", "Pitt", 1, 2);
 
 INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
-VALUES ()
+VALUES (2, "Noe", "Monsivais", 4, 0);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Juan", "Alvarado", 3, 5);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Felicia", "Day", 4, 0);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Alec", "Up", 5, 0);
+
+INSERT INTO employee (emp_id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Ricardo", "Arjona", 5, 7);
